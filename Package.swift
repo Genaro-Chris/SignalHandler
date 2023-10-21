97368e6f544424ac1fab0839e0970b9680f3df39
Genaro-Chris <christian25589@gmail.com>
2023-10-21 23:08:16 +0100

    minor fix v07

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "SignalHandler",
     platforms: [
-      .custom("Linux", versionString: "1.0.0")  
+      .custom("Linux", versionString: "1.0.0"),
+      .macOS(.v10_15)  
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
