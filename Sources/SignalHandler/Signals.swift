967af3a031537753dd6122b04cf7f1e89bdde5ac
Genaro-Chris <christian25589@gmail.com>
2023-11-04 14:37:22 +0100

    minor correction

--- Sources/SignalHandler/Signals.swift
+++ Sources/SignalHandler/Signals.swift
@@ -72,69 +72,71 @@ public enum Signals: Int32, CaseIterable, Sendable, Equatable {
 
 extension Signals: CustomStringConvertible {
     public var description: String {
-        return switch self {
+        let description: String
+        switch self {
 
-            case .SIGHUP: "Hang-up (POSIX)"
+            case .SIGHUP: description = "Hang-up (POSIX)"
 
-            case .SIGINT: "Ctrl + C or Terminal Interrupt (ANSI)"
+            case .SIGINT: description = "Ctrl + C or Terminal Interrupt (ANSI)"
 
-            case .SIGQUIT: "Ctrl + Z or Terminal quit (POSIX)"
+            case .SIGQUIT: description = "Ctrl + Z or Terminal quit (POSIX)"
 
-            case .SIGILL: "Illegal instruction (ANSI)"
+            case .SIGILL: description = "Illegal instruction (ANSI)"
 
-            case .SIGTRAP: "Trace trap (POSIX)"
+            case .SIGTRAP: description = "Trace trap (POSIX)"
 
-            case .SIGIOT: "IOT trap (4.2 BSD)"
+            case .SIGIOT: description = "IOT trap (4.2 BSD)"
 
-            case .SIGBUS: "BUS error (4.2 BSD)"
+            case .SIGBUS: description = "BUS error (4.2 BSD)"
 
-            case .SIGFPE: "Floating point exception (ANSI)"
+            case .SIGFPE: description = "Floating point exception (ANSI)"
 
-            case .SIGUSR1: "User-defined signal 1 (POSIX)"
+            case .SIGUSR1: description = "User-defined signal 1 (POSIX)"
 
-            case .SIGSEGV: "Invalid memory segment access (ANSI)"
+            case .SIGSEGV: description = "Invalid memory segment access (ANSI)"
 
-            case .SIGUSR2: "User-defined signal 2 (POSIX)"
+            case .SIGUSR2: description = "User-defined signal 2 (POSIX)"
 
-            case .SIGPIPE: "Broken pipe (POSIX)"
+            case .SIGPIPE: description = "Broken pipe (POSIX)"
 
-            case .SIGALRM: "Alarm clock (POSIX)"
+            case .SIGALRM: description = "Alarm clock (POSIX)"
 
-            case .SIGTERM: "Termination (ANSI)"
+            case .SIGTERM: description = "Termination (ANSI)"
 
-            case .SIGSTKFLT: "Stack Fault"
+            case .SIGSTKFLT: description = "Stack Fault"
 
-            case .SIGCHLD: "Child process exited or stopped (POSIX)"
+            case .SIGCHLD: description = "Child process exited or stopped (POSIX)"
 
-            case .SIGCONT: "Resume execution if stopped (POSIX)"
+            case .SIGCONT: description = "Resume execution if stopped (POSIX)"
 
-            case .SIGTSTP: #"Ctrl + \ or Terminal stop signal (POSIX"#
+            case .SIGTSTP: description = #"Ctrl + \ or Terminal stop signal (POSIX"#
 
-            case .SIGTTIN: "Background process trying to read, from TTY (POSIX)"
+            case .SIGTTIN: description = "Background process trying to read, from TTY (POSIX)"
 
-            case .SIGTTOU: "Background process trying to write, to TTY (POSIX)"
+            case .SIGTTOU: description = "Background process trying to write, to TTY (POSIX)"
 
-            case .SIGURG: "Urgent condition on socket (4.2 BSD)"
+            case .SIGURG: description = "Urgent condition on socket (4.2 BSD)"
 
-            case .SIGXCPU: "CPU limit exceeded (4.2 BSD)"
+            case .SIGXCPU: description = "CPU limit exceeded (4.2 BSD)"
 
-            case .SIGXFSZ: "File size exceeded (4.2 BSD)"
+            case .SIGXFSZ: description = "File size exceeded (4.2 BSD)"
 
-            case .SIGVTALRM: "Virtual alarm clock (4.2 BSD)"
+            case .SIGVTALRM: description = "Virtual alarm clock (4.2 BSD)"
 
-            case .SIGPROF: "Profiling alarm clock (4.2 BSD"
+            case .SIGPROF: description = "Profiling alarm clock (4.2 BSD"
 
-            case .SIGWINCH: "Window size change (4.2 BSD, Sun)"
+            case .SIGWINCH: description = "Window size change (4.2 BSD, Sun)"
 
-            case .SIGIO: "I/O now possible (4.2 BSD)"
+            case .SIGIO: description = "I/O now possible (4.2 BSD)"
 
-            case .SIGPWR: "Power failure restart (System V)"
+            case .SIGPWR: description = "Power failure restart (System V)"
 
-            case .SIGSYS: "Not defined on any architecture"
+            case .SIGSYS: description = "Not defined on any architecture"
 
-            case .SIGSTKSZ: "SIGSTKSZ"
+            case .SIGSTKSZ: description = "SIGSTKSZ"
 
         }
+        return description
     }
 
 }
